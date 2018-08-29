dd07731047ba97c9e0e1bbc9f40f51438f821fec
Kath <kathleen.cram@dmail.dixie.edu>
2018-08-29 14:09:47 -0600

    styling changes

--- main.js
+++ main.js
@@ -2,30 +2,49 @@ console.log('hello world');
 var myInputs = [];
 
 myResponses = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes - definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
     'Yes',
-    'No',
-    'Maybe',
-    'Yes, Definitely',
+    'Signs point to yes',
+    'Reply hazy, try again',
     'Ask again later',
-    'It is unclear',
-    'Never'
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don\'t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outllook not so good',
+    'Very doubtful',
+    'No'
 ]
 
 var submitQuestion = document.querySelector('#submitQuestion')
+var errorParagraph = document.querySelector('#error');
+var userInput = document.querySelector('#userInput').value
+
 
 submitQuestion.onclick = function () {
-    console.log('button clicked')
-    var randomAnswer = myResponses[Math.floor(Math.random() * myResponses.length)]
-    console.log(randomAnswer)
-    var printAnswer = document.querySelector('#answer')
-    var userInput = document.querySelector('#userInput').value
-    console.log('this is the user input ' ,userInput)
-    printAnswer.innerHTML = randomAnswer
-    var objectPair = {
-        userInput : randomAnswer
+    if (userInput != "") {
+        //errorParagraph.style.display = 'none';
+        var randomAnswer = myResponses[Math.floor(Math.random() * myResponses.length)]
+        var printAnswer = document.querySelector('#answer')
+        var userInput = document.querySelector('#userInput').value
+        printAnswer.innerHTML = randomAnswer
+        
+        
+    } else {
+        errorParagraph.style.display = 'inline';
+        errorParagraph.innerHTML = 'Text box can not be empty';
+        errorParagraph.style.color = 'red';
     }
-    myInputs.push(objectPair)
-    console.log(myInputs); 
+
+    
 }
 
 var hiddenContent = document.querySelector('.hiddenContent');
